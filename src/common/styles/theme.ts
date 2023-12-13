c793934554b75f7d392442d8015ac10a90a2880e
Matheus <ti.matheuscunha@gmail.com>
2023-12-13 09:01:49 -0300

    feat: project start, basic configs, folder structure

---
+++ src/common/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  color: {
+    gray1: "#F4F3F6",
+    gray2: "#E1DEE8",
+    gray3: "#C9C5D4",
+    gray4: "#666173",
+    gray5: "#151516",
+
+    primaryBlue: "#191847",
+    secondaryOrange: "#F5850B",
+
+    white: "#FFFFFF",
+  },
+
+  font: {
+    primary: "'DM Sans', sans-serif",
+  },
+};
